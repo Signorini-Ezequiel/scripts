40550cce8778fb1500ccbe3418ea05e5fd1664ad
Ezequiel <ezequiel.signorini@gmail.com>
2023-07-10 11:34:59 -0300

    add posibility to include label in rename and exclude background images in compressor

--- compressor.py
+++ compressor.py
@@ -12,8 +12,8 @@ if current_path == "h":
 else:
     current_path = input("Enter the path:\n")
 
-print("\n Compressor of images, enter the name of the directory or leave empty to save in the current path\n")
-destiny_path = input("Enter the name of the destiny directory:\n")
+
+destiny_path = input("Enter the name of the destiny directory (empty for here):\n")
 image_path = os.path.join(current_path, destiny_path)
 
 if destiny_path != "":
@@ -24,7 +24,7 @@ if destiny_path != "":
             while new_destiny != "n"  and new_destiny != "y":
                 new_destiny = input("The chosen directory name already exists, change destiny (y/n): ")
             if new_destiny == "y":
-                destiny_path = input("Enter the name of the directory to put the pictures:\n")
+                destiny_path = input("Enter the name of the destiny directory (empty for here):\n")
                 os.makedirs(image_path)
             else:
                 break
@@ -35,27 +35,45 @@ else:
     print("Everything will be added in the current path\n")
 
 
-image_quality = int(input("\nChoose an image quality in percentage : "))
+image_quality = ""
+while "" == image_quality:
+    try:
+        image_quality = int(input("Choose an image quality in percentage : "))
+    except:
+        print("The entered value is not a number, enter a number\n")
+        image_quality = ""
+
+label = ""
+while "" == label:
+    label = input("Should we add a label to the compressed picture (y/n): ")
+    if "y" != label and "n" != label:
+        print("Enter (y/n)\n")
+        label = ""
+if "y" == label:
+    label = "-comp"
 
 
 
+print("\n\n--------------------------------------")
 for name in os.listdir(current_path):
     name, extension = os.path.splitext(name)
 
     if extension in [".jpg", ".jpeg", ".png", ".bmp", ".webp"]:
-        # Abro la imagen y la transformo a RBG para manipularla
-        picture = Image.open(f'{current_path}/{name}{extension}')
-        picture = picture.convert('RGB')
+        if "background" not in name:
+            # Abro la imagen y la transformo a RBG para manipularla
+            picture = Image.open(f'{current_path}/{name}{extension}')
+            picture = picture.convert('RGB')
 
-        try:
-            # Obtengo el tamaño de la imagen
-            # size = os.path.getsize(f'{current_path}/{name}{extension}')
-            # print(f'Tamaño original de la imagen {name}: {size}\n')
-            # size = size/image_quality
-            # print(f'Tamaño final de la imagen {name}: {size}\n')
+            try:
+                # Obtengo el tamaño de la imagen
+                # size = os.path.getsize(f'{current_path}/{name}{extension}')
+                # print(f'Tamaño original de la imagen {name}: {size}\n')
+                # size = size/image_quality
+                # print(f'Tamaño final de la imagen {name}: {size}\n')
 
-            # Guardo la imagen de forma optimizada
-            picture.save(os.path.join(image_path, name + '.jpg'), optimize=True, quality=image_quality)
-            print(f'The image {name} were saved')
-        except Exception as error:
-            print(f'{error}\n')
+                # Guardo la imagen de forma optimizada
+                picture.save(os.path.join(image_path, name + label +'.jpg'), optimize=True, quality=image_quality)
+                print(f'The image {name + label} were saved')
+            except Exception as error:
+                print(f'{error}\n')
+print("--------------------------------------\n")
